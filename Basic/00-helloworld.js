6859bb79620878e2ecb5c00552974b8a99c61642
aronaldo9 <aaronpesqueirafarina@gmail.com>
2024-07-10 23:57:05 +0200

    hola mundo y variables

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,27 @@
+// Ésto es un comentario simple
+
+/*Esto es
+ * un comentario en
+ * varias líneas
+ */
+
+console.log("¡Hola, Javascript!");
+console.log("¡Hola, Javascript!");
+console.log(`¡Hola, Javascript!`);
+
+// console.log(`¡Hola, Javascript!`);
+
+/*
+console.log("¡Hola, Javascript!");
+console.log("¡Hola, Javascript!");
+console.log(`¡Hola, Javascript!`);
+*/
+
+console.log("5");
+console.log(5);
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(5 ** 2);
---
+++ Basic/01-variables.js
@@ -0,0 +1,24 @@
+// var
+
+var helloWorld = "¡Hola, Javascript!";
+console.log(helloWorld);
+
+helloWorld = "¡Hola de nuevo, Javascript!";
+console.log(helloWorld);
+
+// let
+
+let helloWorld2 = "¡Hola, Javascript 2!";
+console.log(helloWorld2);
+
+helloWorld2 = "¡Hola de nuevo, Javascript 2!";
+console.log(helloWorld2);
+
+// const
+
+const helloWorld3 = "¡Hola, Javascript 3!";
+console.log(helloWorld3);
+
+// Ésto va a dar error porque una constante no se puede reasignar
+// helloWorld3 = "¡Hola de nuevo, Javascript 3!";
+// console.log(helloWorld3);
